397993f32891cfd9bceb38273fe509eb04eb65de
M M Kishore <kishoremeenakshi2001@gmail.com>
2021-09-17 06:46:15 +0530

    Level Order Added

---
+++ LevelOrder.js
@@ -0,0 +1,37 @@
+https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/use-breadth-first-search-in-a-binary-search-tree
+
+var displayTree = tree => console.log(JSON.stringify(tree, null, 2));
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+function BinarySearchTree() {
+  this.root = null;
+  this.levelOrder = (root = this.root) => {
+    if(!root) return null;
+    let queue = [root];
+    let results = [];
+    while(queue.length > 0) {
+      let node = queue.shift();
+      results.push(node.value);
+      if(node.left) queue.push(node.left);
+      if(node.right) queue.push(node.right);
+    }
+    return results;
+  }
+
+  this.reverseLevelOrder = (root = this.root) => {
+    if(!root) return null;
+    let queue = [root];
+    let results = [] ;
+    while ( queue.length > 0) {
+      let node = queue.shift();
+      results.push(node.value);
+      if(node.right) queue.push(node.right);
+      if(node.left ) queue.push(node.left);
+    }
+    return results;
+  }
+}
