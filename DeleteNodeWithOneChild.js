402329371e998acb75cfdb68666a15429e4325dc
M M Kishore <kishoremeenakshi2001@gmail.com>
2021-09-18 12:54:31 +0530

    Added DeleteNodeWithOneChild Added TwoSum from leetcode

---
+++ DeleteNodeWithOneChild.js
@@ -0,0 +1,63 @@
+https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/delete-a-node-with-one-child-in-a-binary-search-tree
+
+var displayTree = tree => console.log(JSON.stringify(tree, null, 2));
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+function BinarySearchTree() {
+  this.root = null;
+  this.remove = function(value) {
+    if (this.root === null) {
+      return null;
+    }
+    var target;
+    var parent = null;
+    // Find the target value and its parent
+    (function findValue(node = this.root) {
+      if (value == node.value) {
+        target = node;
+      } else if (value < node.value && node.left !== null) {
+        parent = node;
+        return findValue(node.left);
+      } else if (value < node.value && node.left === null) {
+        return null;
+      } else if (value > node.value && node.right !== null) {
+        parent = node;
+        return findValue(node.right);
+      } else {
+        return null;
+      }
+    }.bind(this)());
+    if (target === null) {
+      return null;
+    }
+    // Count the children of the target to delete
+    var children =
+      (target.left !== null ? 1 : 0) + (target.right !== null ? 1 : 0);
+    // Case 1: Target has no children
+    if (children === 0) {
+      if (target == this.root) {
+        this.root = null;
+      } else {
+        if (parent.left == target) {
+          parent.left = null;
+        } else {
+          parent.right = null;
+        }
+      }
+    }
+    // Case 2: Target has one child
+    // Only change code below this line
+    if(children === 1){
+      if(target === this.root){
+        if(target.left === null){
+          this.root = target.left
+        }
+        this.root = target.right
+      }
+    }
+  };
+}
