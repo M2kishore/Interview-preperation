9aac96d90f1bdc87c615f80ddb1a55f229aa9783
M M Kishore <60045529+M2kishore@users.noreply.github.com>
2022-02-14 11:27:03 +0530

    Create MinCostClimbingStairs.js

---
+++ Leetcode Study Plan/dp/MinCostClimbingStairs.js
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/min-cost-climbing-stairs/
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    for (let i = cost.length - 3; ~i; i--)
+        {
+        cost[i] += Math.min(cost[i+1], cost[i+2]);
+            console.log(~i);
+        }
+    return Math.min(cost[0], cost[1]);
+};
