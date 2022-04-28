c86879729ade58c71bfe132d3bd07e8f07571db5
M M Kishore <60045529+M2kishore@users.noreply.github.com>
2022-04-28 12:02:23 +0530

    Create binarySearch.js

---
+++ Leetcode Study Plan/Binary Search/binarySearch.js
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/binary-search/submissions/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let low = 0;
+    let high = nums.length-1;
+    while(low<=high){
+        let mid = low+(high-low);
+        let midElement = nums[mid];
+        if(midElement===target){
+            return mid;
+        }
+        else if(midElement<target){
+            low = mid+1;
+        }else{
+            high = mid-1;
+        }
+    }
+    return -1;
+};
