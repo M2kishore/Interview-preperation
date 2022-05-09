2400fb09f28056543dac7c5e21fadc0275a51277
M M Kishore <60045529+M2kishore@users.noreply.github.com>
2022-05-09 20:07:55 +0530

    Create GenerateParenthesis.js

---
+++ GenerateParenthesis.js
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/generate-parentheses/submissions/
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let result = [];
+    backtracking(result,"",0,0,n);
+    return result;
+};
+var backtracking = (result,currentString,open,close,max)=>{
+    if(currentString.length === 2*max){
+        result.push(currentString);
+        return
+    }
+    if(open<max) backtracking(result,currentString+"(",open+1,close,max);
+    if(close<open) backtracking(result,currentString+")",open,close+1,max);
+}
