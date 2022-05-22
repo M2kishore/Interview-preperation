c772024cd47e3777d7427701bcdf35cc82c5ea60
M M Kishore <60045529+M2kishore@users.noreply.github.com>
2022-05-22 21:52:23 +0530

    Create TwoSum.js

---
+++ stiver's SDE sheet/TwoSum.js
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/two-sum/submissions/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let holder = {};
+    for(let i=0;i<nums.length;i++){
+        if(target-nums[i] in holder){
+            return [holder[target-nums[i]],i]
+        }else{
+            holder[nums[i]] = i;
+        }
+    }
+};
