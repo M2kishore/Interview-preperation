a4f43b7b065cec4bd94f9e09c2f78f713a35226e
M M Kishore <60045529+M2kishore@users.noreply.github.com>
2022-05-24 21:50:54 +0530

    Create RemoveNthNodeFromEndOfList.js

---
+++ stiver's SDE sheet/RemoveNthNodeFromEndOfList.js
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+   let start = new ListNode()
+    start.next = head
+    
+    let fast = start
+    let slow = start
+    
+    for (let i = 0; i <= n; i += 1) {
+        fast = fast.next
+    }
+    
+    while (fast !== null) {
+        slow = slow.next
+        fast = fast.next
+    }
+        
+    slow.next = slow.next.next
+    
+    return start.next
+};
