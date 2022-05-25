0c551466f79b488c49d7886c3d19f62f40be3a05
M M Kishore <60045529+M2kishore@users.noreply.github.com>
2022-05-25 10:35:08 +0530

    Create AddTwoNumbers.js

---
+++ stiver's SDE sheet/AddTwoNumbers.js
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/add-two-numbers/submissions/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let carry = 0;
+    let head = null;
+    let root = null;
+    while(l1 || l2){
+        let sum = 0;
+        if(l1){
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if(l2){
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        sum += carry;
+        let val = sum % 10;
+        carry = Math.floor(sum/10);
+        if(head){
+            head.next = new ListNode(val);
+            head = head.next;
+        }else{
+            head = new ListNode(val);
+            root = head;
+        }
+    }
+    if(carry){
+        head.next = new ListNode(carry);
+        head = head.next;
+    }
+    return root;
+};
