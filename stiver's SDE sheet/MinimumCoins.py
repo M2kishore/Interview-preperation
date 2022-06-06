6dd97cd8984c47e99c81a5db9fb6c2ed014c45df
M M Kishore <60045529+M2kishore@users.noreply.github.com>
2022-06-07 01:29:50 +0530

    Update MinimumCoins.py

--- stiver's SDE sheet/MinimumCoins.py
+++ stiver's SDE sheet/MinimumCoins.py
@@ -1,4 +1,4 @@
-
+#https://www.codingninjas.com/codestudio/problems/975277?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=1
 denominations = [1, 2, 5, 10, 20, 50, 100, 500, 1000]
 
 
