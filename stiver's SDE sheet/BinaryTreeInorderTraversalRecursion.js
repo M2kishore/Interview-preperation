3c56bbb984c6c48a5860a9b8c827eac9720c65ef
M M Kishore <60045529+M2kishore@users.noreply.github.com>
2022-06-30 22:33:52 +0530

    Create BinaryTreeInorderTraversalRecursion.js

---
+++ stiver's SDE sheet/BinaryTreeInorderTraversalRecursion.js
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if(!root){
+          return [];
+    }
+    return [...inorderTraversal(root.left),root.val,...inorderTraversal(root.right)];
+};
